bda981e9444da1b3259303496cc05d659ba81438
JP <joaobianchi@live.com>
2024-01-16 19:06:17 -0300

    :tada: setting up repo

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+
 contract NFT is ERC721URIStorage {
     uint public tokenCount;
-    constructor() ERC721("DApp NFT", "DAPP"){}
+
+    constructor() ERC721("DApp NFT", "DAPP") {}
+
     function mint(string memory _tokenURI) external returns(uint) {
-        tokenCount ++;
+        tokenCount++;
         _safeMint(msg.sender, tokenCount);
         _setTokenURI(tokenCount, _tokenURI);
+
         return(tokenCount);
     }
 }
--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -7,13 +7,17 @@ async function main() {
   
   // Get the ContractFactories and Signers here.
   const NFT = await ethers.getContractFactory("NFT");
-  const Marketplace = await ethers.getContractFactory("Marketplace");
-  // deploy contracts
-  const marketplace = await Marketplace.deploy(1);
   const nft = await NFT.deploy();
+
+  const Marketplace = await ethers.getContractFactory("Marketplace");
+  const marketplace = await Marketplace.deploy(1);
+
+  console.log("NFT contract address: ", nft.address);
+  console.log("Marketplace contract address: ", marketplace.address);
+
   // Save copies of each contracts abi and address to the frontend.
-  saveFrontendFiles(marketplace , "Marketplace");
   saveFrontendFiles(nft , "NFT");
+  saveFrontendFiles(marketplace , "Marketplace");
 }
 
 function saveFrontendFiles(contract, name) {
